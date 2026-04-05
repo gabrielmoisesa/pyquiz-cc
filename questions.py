2030b6370bdaf4ca589023b9fdf7b9e3c6db28f0
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-05 06:43:04 -0300

    feat: create questions.py to handle questions data and rename quiz.py to main.py

---
+++ questions.py
@@ -0,0 +1,20 @@
+questions = [
+    {
+        "question": "O que é um algoritmo?",
+        "options": {
+            "a": "É perceber semelhanças entre problemas ou situações.",
+            "b": "É uma sequência finita, lógica e bem definida de instruções para resolver um problema.",
+            "c": "É focar no que é importante e ignorar detalhes desnecessários naquele momento.",
+        },
+        "answer": "b"
+    },
+    {
+        "question": "O que é uma variável?",
+        "options": {
+            "a": "Um tipo de loop.",
+            "b": "Um espaço na memória para armazenar um valor.",
+            "c": "Uma função matemática.",
+        },
+        "answer": "b"
+    },
+]
