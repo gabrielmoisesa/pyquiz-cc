9e5b1e428411ca8b4ff99b00e3741f3051ec48b0
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-05 06:36:28 -0300

    feat: create first question starting logic

--- quiz.py
+++ quiz.py
@@ -13,4 +13,20 @@ valid_asnwers = ["sim", "s", "yes", "y", "true", "claro", "com certeza"]
 if playing not in valid_asnwers:
     quit()
 
-print("testado!")
+question_01 = (
+    "O que é um algoritmo?\n"
+
+    "a) É perceber semelhanças entre problemas ou situações.\n"
+
+    "b) É uma sequência finita, lógica e bem "
+    "definida de instruções, regras ou "
+    "passos, para resolver um problema "
+    "ou realizar uma tarefa específica.\n"
+
+    "c) É focar no que é importante e ignorar detalhes "
+    "desnecessários naquele momento.\n"
+
+    "a, b ou c? "
+)
+user_answer_01 = input(question_01).lower().strip()
+correct_answer_01 = "c"
