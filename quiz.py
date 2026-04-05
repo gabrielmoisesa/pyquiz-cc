3a06301c4a8abaa3cf2775a7fb1fb8dfe32cddee
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-05 20:33:43 -0300

    refactor: change some var names

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ jogando = input("Gostaria de iniciar?(s/n) ").lower().strip()
 if jogando != "s" and jogando != "sim":
     quit()
 
-opcoes_de_resposta = "Sua resposta(a/b/c/d): "
+opcoes_resposta = "Sua resposta(a/b/c/d): "
 
 # ==========
 # Questão 1    
@@ -33,4 +33,4 @@ resposta_01 = "b"
 
 print(questao_01)
 
-user_answer_01 = input(opcoes_de_resposta).lower().strip()
+resposta_usuario_01 = input(opcoes_resposta).lower().strip()
