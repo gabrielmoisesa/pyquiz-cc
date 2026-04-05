d48ee99e9f6264f6e9f6b592bae5a66cbc168c2c
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-04-05 06:16:43 -0300

    feat: create start quiz question

--- quiz.py
+++ quiz.py
@@ -6,3 +6,11 @@ intro_message = (
 border =  "=" * 40
 
 print(f"{border}\n{intro_message}\n{border}")
+
+playing = input("Gostaria de iniciar?(S/N) ").lower().strip()
+valid_asnwers = ["sim", "s", "yes", "y", "true", "claro", "com certeza"]
+
+if playing not in valid_asnwers:
+    quit()
+
+print("testado!")
